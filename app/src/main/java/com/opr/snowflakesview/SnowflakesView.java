383bc025a014d7da31ef628be0a73a99ec25003a
OptimusPrimeRen <1014732168@qq.com>
2016-08-18 21:36:50 +0800

    add some notes.

--- app/src/main/java/com/opr/snowflakesview/SnowflakesView.java
+++ app/src/main/java/com/opr/snowflakesview/SnowflakesView.java
@@ -1,19 +1,12 @@
 package com.opr.snowflakesview;
 
-import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
-import android.os.HandlerThread;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
-import java.util.IllegalFormatCodePointException;
 import java.util.Random;
 
 /**
@@ -34,6 +27,7 @@ public class SnowflakesView extends View {
     private int mSnowCount; //雪花总数量
 
     private enum FallSnowState {START, PAUSE, RUNNING, STOP}
+
     private FallSnowState mFallSnowState;
 
 
@@ -53,7 +47,9 @@ public class SnowflakesView extends View {
 
     private void initAttrs(Context context, AttributeSet attrs, int defStyleAttr) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SnowflakesView);
+        //下落速度
         mFallSpeed = typedArray.getFloat(R.styleable.SnowflakesView_fallSpeed, (float) 0.1);
+        //下落雪花个数
         mSnowCount = typedArray.getInteger(R.styleable.SnowflakesView_snowCount, 10);
         typedArray.recycle();
         judgeAttrIsReasonable();
@@ -72,13 +68,16 @@ public class SnowflakesView extends View {
 
     public void initVariable() {
         mRandom = new Random();
+        //此处使用Drawable对象而不是Bitmap，原因是：Drawable提供setAlpha方法，而Bitmap没有。当然，也可以通过设置canvas的alpha，不过测试表示效率较低。
         mSnowFlowerDrawable = getResources().getDrawable(R.drawable.image_cpu_cooler_snow);
+        //获取图片原始高度，用于之后计算
         mSnowFlowerHeight = mSnowFlowerDrawable.getIntrinsicHeight();
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
+        //整个view的宽高
         mSnowViewWidth = r - l;
         mSnowViewHeight = b - t;
     }
@@ -93,6 +92,7 @@ public class SnowflakesView extends View {
 
         for (Snow snow : mSnows) {
             canvas.save();
+            //通过设置Drawable边界，从而Drawable会自动对其中图片进行缩放，注意：这里的四个参数，分别是图片在整个view（canvas）中的顶点
             mSnowFlowerDrawable.setBounds((int) snow.mX, (int) snow.mY, (int) (snow.mX + mSnowFlowerHeight * snow.mScale), (int) (snow.mY + mSnowFlowerHeight * snow.mScale));
             mSnowFlowerDrawable.setAlpha(snow.mAlpha);
             mSnowFlowerDrawable.draw(canvas);
@@ -109,9 +109,10 @@ public class SnowflakesView extends View {
         }
     }
 
+    //单个雪花对象，用于保存雪花的状态
     private class Snow {
         public float mScale;
-        public int mAlpha; //appha
+        public int mAlpha; //alpha
         public float mX;
         public float mY;
 
@@ -128,14 +129,17 @@ public class SnowflakesView extends View {
 
         //caculate the snow's next attrs , include position,size etc. 计算下一个雪花的相关参数  包括位置  大小等
         public void calculateNextAttr() {
+            //很简单，当监测到雪花落到view底部时，重新计算初始值。
             if (mY > mSnowViewHeight / mScale) {
                 init();
             } else {
+                //没到底部，以一定速率下降，这里乘以mScale使得越大的落得越快
                 mY = mY + mScale * mSnowFlowerHeight * mFallSpeed;
             }
         }
     }
 
+    //开始下雪
     public void startSnow() {
         mFallSnowState = FallSnowState.START;
 
@@ -156,18 +160,21 @@ public class SnowflakesView extends View {
 
     }
 
+    //暂停下雪
     public void pauseSnow() {
         if (mFallSnowState == FallSnowState.RUNNING) {
             mFallSnowState = FallSnowState.PAUSE;
         }
     }
 
+    //继续下雪
     public void resumeSnow() {
         if (mFallSnowState == FallSnowState.PAUSE) {
             mFallSnowState = FallSnowState.RUNNING;
         }
     }
 
+    //停止下雪
     public void stopSnow() {
         mFallSnowState = FallSnowState.STOP;
     }
@@ -181,6 +188,9 @@ public class SnowflakesView extends View {
                         calculateSnowsNextAttr();
                         postInvalidate();
                         try {
+                            //大约30帧的动画速度，不会太多冗余计算与刷新
+                            //当然，如果4.0以上，可以使用valueAnimator，不需新开线程，损耗较小。
+                            //也可以使用HandlerThread，Timer等，不过本质上都是线程。
                             Thread.sleep(30);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
